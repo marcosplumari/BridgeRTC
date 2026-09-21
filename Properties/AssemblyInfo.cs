f6928b4f60e2d2255621cb9950554eebd85213c7
marcosplumari <marplumari@gmail.com>
2026-09-21 08:51:30 -0300

    feat: adiciona Properties/AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SplitPaymentBridge")]
+[assembly: AssemblyDescription("Ponte COM Interop para Split Payment e Vinculação de Pagamentos no DF-e")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sua Empresa")]
+[assembly: AssemblyProduct("SplitPaymentBridge")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Define se os tipos deste assembly serao visíveis para componentes COM (VB6)
+[assembly: ComVisible(true)]
+
+// GUID exclusivo do assembly
+[assembly: Guid("C3D4E5F6-A7B8-4C9D-0E1F-2A3B4C5D6E7F")]
+
+// Versao do assembly
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
