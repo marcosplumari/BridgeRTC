75357159a05f30f436e1e05b067ecf1ab1e3dff3
marcosplumari <marplumari@gmail.com>
2026-09-21 09:42:41 -0300

    feat: atualiza Properties/AssemblyInfo.cs para BridgeRTC

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,21 +1,17 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("SplitPaymentBridge")]
-[assembly: AssemblyDescription("Ponte COM Interop para Split Payment e Vinculação de Pagamentos no DF-e")]
+[assembly: AssemblyTitle("BridgeRTC")]
+[assembly: AssemblyDescription("Ponte COM Interop em C# (.NET 4.8) para Reforma Tributária do Consumo e Split Payment")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sua Empresa")]
-[assembly: AssemblyProduct("SplitPaymentBridge")]
+[assembly: AssemblyProduct("BridgeRTC")]
 [assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Define se os tipos deste assembly serao visíveis para componentes COM (VB6)
 [assembly: ComVisible(true)]
-
-// GUID exclusivo do assembly
 [assembly: Guid("C3D4E5F6-A7B8-4C9D-0E1F-2A3B4C5D6E7F")]
 
-// Versao do assembly
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
